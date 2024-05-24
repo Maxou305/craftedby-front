286652c6cdf33defc094c595ab15343c6fcef9a8
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-05-24 14:35:55 +0200

    Router setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createPinia} from "pinia";
-
+import { createPinia } from 'pinia'
+import router from '@/pages/routes.js'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.use(router)
 app.mount('#app')
