5263b5d5113c18616363b9d701f43bead22821cd
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-05-27 10:17:46 +0200

    tailwind setup updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,22 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      fontSize: {
+        'text-light': ['16px', '1.5'],
+        'text-normal': ['18px', '1.5'],
+        bold: ['22px', '1.5'],
+        title: ['32px', '1.25'],
+        'shop-title': ['64px', '1.25'],
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
-
