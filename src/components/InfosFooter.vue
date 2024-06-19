37071694783648d4d14ec8a4d38a0a9cb0ad9679
lpzkjdoi <lpzkjdoi@icloud.com>
2024-06-19 15:45:33 +0200

    InfosFooter.vue is now responsive

--- src/components/InfosFooter.vue
+++ src/components/InfosFooter.vue
@@ -20,15 +20,16 @@ const footerData = [
 </script>
 
 <template>
-  <div class="flex items-center justify-around flex-col gap-8 py-12">
-    <div class="w-[1440px] flex justify-around">
+  <div class="flex flex-col items-center justify-around gap-8 py-12">
+    <div class="flex max-w-[1440px] flex-col justify-around lg:flex-row">
       <div
-        class="w-80 flex flex-col text-center gap-4"
+        class="flex w-80 flex-col gap-4 text-center"
         v-for="data in footerData"
+        :key="data.title"
       >
         <h3>{{ data.title }}</h3>
-        <ul class="flex justify-center flex-col gap-2">
-          <li v-for="link in data.links">
+        <ul class="flex flex-col justify-center gap-2">
+          <li v-for="link in data.links" :key="link">
             <RouterLink to="">
               {{ link }}
             </RouterLink>
@@ -39,7 +40,7 @@ const footerData = [
     <img src="../assets/images/icons/logo-icon.svg" alt="logo" />
     <div class="flex gap-4">
       <div
-        class="w-[50px] h-[50px] bg-neutral-50 rounded-full border border-zinc-200 flex items-center justify-center"
+        class="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-zinc-200 bg-neutral-50"
       >
         <a
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D"
@@ -52,7 +53,7 @@ const footerData = [
         </a>
       </div>
       <div
-        class="w-[50px] h-[50px] bg-neutral-50 rounded-full border border-zinc-200 flex items-center justify-center"
+        class="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-zinc-200 bg-neutral-50"
       >
         <a
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D"
@@ -65,7 +66,7 @@ const footerData = [
         </a>
       </div>
       <div
-        class="w-[50px] h-[50px] bg-neutral-50 rounded-full border border-zinc-200 flex items-center justify-center"
+        class="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-zinc-200 bg-neutral-50"
       >
         <a
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D"
@@ -78,7 +79,7 @@ const footerData = [
         </a>
       </div>
     </div>
-    <div class="flex text-center flex-col">
+    <div class="flex flex-col text-center">
       <p>© 2024 CraftedBy - Conception BestTeam</p>
       <a href="">Politique de confidentialité</a>
     </div>
