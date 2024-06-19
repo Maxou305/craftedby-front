5cb71c615749da3335c6681347e6bd0c76a83508
lpzkjdoi <lpzkjdoi@icloud.com>
2024-06-19 16:00:33 +0200

    padding added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,7 +7,7 @@ import CategoryList from '@/components/CategoryList.vue'
 </script>
 
 <template>
-  <div class="m-auto my-4 mt-24 flex max-w-[1440px] flex-wrap gap-4">
+  <div class="m-auto my-4 mt-24 flex max-w-[1440px] flex-wrap gap-4 p-4">
     <HomeBanner />
     <ProductList />
     <ShopList />
