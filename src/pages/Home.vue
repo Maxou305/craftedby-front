4330ba5fbcb975c04a380d4f118413c90f0a8e9b
lpzkjdoi <lpzkjdoi@icloud.com>
2024-06-19 15:22:42 +0200

    Home.vue is now responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,7 +7,7 @@ import CategoryList from '@/components/CategoryList.vue'
 </script>
 
 <template>
-  <div class="mt-24 w-[1440px] m-auto flex flex-wrap gap-4 my-4">
+  <div class="m-auto my-4 mt-24 flex max-w-[1440px] flex-wrap gap-4">
     <HomeBanner />
     <ProductList />
     <ShopList />
