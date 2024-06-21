7f0c0b1ce55efc1a6145d28b89e51f4452417f28
lpzkjdoi <lpzkjdoi@icloud.com>
2024-06-21 14:18:59 +0200

    ProductList.vue is now more generic (Home.vue updated too)

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,21 +1,22 @@
 <script setup>
 import ProductCard from '@/components/ProductCard.vue'
-import { useProductsStore } from '@/stores.js'
-import { onMounted } from 'vue'
 
-const store = useProductsStore()
-onMounted(() => {
-  store.fetchProducts()
+// const store = useProductsStore()
+// onMounted(() => {
+//   store.fetchProducts()
+// })
+
+const props = defineProps({
+  products: [],
 })
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold">Meilleures ventes</h1>
   <div
-    class="xs:grid-cols-1 grid grid-cols-3 gap-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5"
+    class="grid grid-cols-3 gap-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5"
   >
     <ProductCard
-      v-for="product in store.products"
+      v-for="product in props.products"
       :key="product.id"
       :product="product"
     />
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,12 +4,21 @@ import ProductList from '@/components/ProductList.vue'
 import ShopList from '@/components/ShopList.vue'
 import ShopAd from '@/components/ShopAd.vue'
 import CategoryList from '@/components/CategoryList.vue'
+import { useProductsStore } from '@/stores.js'
+import { onMounted } from 'vue'
+
+const store = useProductsStore()
+
+onMounted(() => {
+  store.fetchProducts()
+})
 </script>
 
 <template>
   <div class="m-auto my-4 mt-24 flex max-w-[1440px] flex-wrap gap-4 p-4">
     <HomeBanner />
-    <ProductList />
+    <h1 class="text-3xl font-bold">Meilleures ventes</h1>
+    <ProductList :products="store.products" />
     <ShopList />
     <ShopAd />
     <CategoryList />
