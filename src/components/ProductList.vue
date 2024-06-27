f9724e9d1b2c12002ea33e3574848fc5accadce8
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-06-27 10:59:07 +0200

    warnings for products type are fixed

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,13 +1,8 @@
 <script setup>
 import ProductCard from '@/components/ProductCard.vue'
 
-// const store = useProductsStore()
-// onMounted(() => {
-//   store.fetchProducts()
-// })
-
-const props = defineProps({
-  products: [],
+defineProps({
+  products: Array,
 })
 </script>
 
@@ -16,7 +11,7 @@ const props = defineProps({
     class="grid grid-cols-3 gap-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5"
   >
     <ProductCard
-      v-for="product in props.products"
+      v-for="product in products"
       :key="product.id"
       :product="product"
     />
